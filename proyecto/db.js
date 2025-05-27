634bea3c679b814470b994e26381323ee4564687
PRED13 <155868501+PRED13@users.noreply.github.com>
2025-05-26 21:46:04 -0600

    Update db.js

--- proyecto/db.js
+++ proyecto/db.js
@@ -1,1 +1,17 @@
-jhadsdhasgdhsgdh
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'carrera'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a MySQL:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos MySQL exitosa.');
+});
+
+module.exports = connection;
